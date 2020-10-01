1c9dcf9319539e76e74e94bafc4f96be8baa9b80
MichaelChorak <70633896+MichaelChorak@users.noreply.github.com>
2020-10-01 10:05:24 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var toggleMenu = document.querySelector('.hamburgerMenu');
+var menu = document.querySelector('#menuItems');
+
+ function toonMenu(){
+     menu.classList.toggle('fadeIn');
+ }
+
+ toggleMenu.addEventListener('click', toonMenu);
